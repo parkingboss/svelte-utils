241d6ea86e06ba5bc6eeec708dcbd25138f540ed
Chris Pfohl <christopher.pfohl@gmail.com>
2020-03-27 08:51:55 -0400

    rename gps statuses and state

--- src/geo.ts
+++ src/geo.ts
@@ -2,36 +2,36 @@ import { readable } from 'svelte/store';
 import { gps } from '@parkingboss/utils';
 import { Canceller } from './Canceller';
 
-export const GPS_UNSTARTED = 'unstarted';
-export const GPS_UNSUPPORTED = 'unsupported';
-export const GPS_REQUESTING = 'requesting';
-export const GPS_UNPERMITTED = 'unpermitted';
-export const GPS_LOADING = 'loading';
-export const GPS_ERROR = 'gps-error';
-export const GPS_ACTIVE = 'active';
+export const UNSTARTED = 'unstarted';
+export const UNSUPPORTED = 'unsupported';
+export const REQUESTING = 'requesting';
+export const UNPERMITTED = 'unpermitted';
+export const LOADING = 'loading';
+export const ERROR = 'gps-error';
+export const ACTIVE = 'active';
 
-export type GpsStatus = typeof GPS_UNSTARTED | typeof GPS_UNSUPPORTED | typeof GPS_REQUESTING | typeof GPS_UNPERMITTED | typeof GPS_LOADING | typeof GPS_ERROR | typeof GPS_ACTIVE;
+export type Status = typeof UNSTARTED | typeof UNSUPPORTED | typeof REQUESTING | typeof UNPERMITTED | typeof LOADING | typeof ERROR | typeof ACTIVE;
 
-interface Geo {
-  status: GpsStatus;
+interface State {
+  status: Status;
   position: Position | null;
   error: PositionError | null;
 }
 
-const DefaultGeo: Geo = { status: GPS_UNSTARTED, position: null, error: null };
+const DefaultGeo: State = { status: UNSTARTED, position: null, error: null };
 
 export const geo = readable(DefaultGeo, set => {
-  function setter(opts: Partial<Geo> = {}) {
+  function setter(opts: Partial<State> = {}) {
     set(Object.assign(geo, opts));
   }
 
   let geo = DefaultGeo;
 
   if (!gps.available) {
-    return setter({ status: GPS_UNSUPPORTED });
+    return setter({ status: UNSUPPORTED });
   }
 
-  setter({ status: GPS_REQUESTING });
+  setter({ status: REQUESTING });
 
   let cancelled = false;
   let canceller: null | Canceller = null;
@@ -39,9 +39,9 @@ export const geo = readable(DefaultGeo, set => {
     .then(permitted => {
       if (cancelled) return;
       if (!permitted) {
-        return setter({ status: GPS_UNPERMITTED });
+        return setter({ status: UNPERMITTED });
       }
-      canceller = gps.watch(position => setter({ status: GPS_ACTIVE, position, error: null  }), error => setter({ status: GPS_ERROR, error }));
+      canceller = gps.watch(position => setter({ status: ACTIVE, position, error: null  }), error => setter({ status: ERROR, error }));
     });
 
   return () => {
